6119694e8b55335ef5e51b3f2ab31915d09524bc
jsadiga <jsadiga@gmail.com>
2013-10-26 15:45:59 -0400

    dev-sprint1

--- exercise54.py
+++ exercise54.py
@@ -1,2 +1,14 @@
 # This is where Exercise 5.4 goes
-# Name:
+# Name:shishira
+
+def is_traingle(a,b,c):
+	if a>b+c:
+		print "No"
+	elif b>a+c:
+		print"No"
+	elif c>b+a:
+		print"No"
+	else:
+		print "Yes"
+
+is_traingle(2,1,1)
